877dcef048ba2856ed336521617b8c4ec76002df
KingShaggy <39702459+Shaggy222@users.noreply.github.com>
2019-03-17 20:12:20 +0100

    Removed the active insulin from this branch

--- app/src/main/java/funkyduck/protonmail/com/insulincalc/MainActivity.java
+++ app/src/main/java/funkyduck/protonmail/com/insulincalc/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         final EditText InsulinFølsomhetEditText;
         InsulinFølsomhetEditText = (EditText) findViewById(R.id.InsulinFølsomhetEditText);
         final EditText InsuEditText;
-        InsuEditText = (EditText) findViewById(R.id.InsuEditText);
 
 
         Button CalculateButton = (Button) findViewById(R.id.CalculateButton);
@@ -45,12 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 TextView InsulindoseTextView = (TextView) findViewById(R.id.InsulindoseTextView);
                 TextView KorreksjonTextView = (TextView) findViewById(R.id.KorreksjonTextView);
                 TextView KarbTextView = (TextView) findViewById(R.id.KarbTextView);
-                TextView InsTidTextView = (TextView) findViewById(R.id.InsTidTextView);
-                Button InsulinButton = (Button) findViewById(R.id.InsulinButton);
-                final TextView AktivInsulinTextView = (TextView) findViewById(R.id.AktivInsulinTextView);
-                final TextView InsuEditText = (EditText) findViewById(R.id.InsuEditText);
                 int zero = 0;
-                AktivInsulinTextView.setText(zero);
 
 
                 //REGNER UT DOSEN
@@ -59,19 +53,13 @@ public class MainActivity extends AppCompatActivity {
                 double blodsukker = Double.parseDouble(BlodsukkerEditText.getText().toString());
                 double karbfaktor = Double.parseDouble(KarbFaktorEditText.getText().toString());
                 double insulinfølsomhet = Double.parseDouble(InsulinFølsomhetEditText.getText().toString());
-                double AktivIns = Double.parseDouble(AktivInsulinTextView.getText().toString());
                 double korre;
 
                 double kar = karb / karbfaktor;
                 String karbdose = String.format("%.1f", kar);
                 double KorrStart = blodsukker - 5;
                 double korr = KorrStart / insulinfølsomhet;
-                if (AktivIns > 0) {
-                    korre = korr - AktivIns;                                            //KRASJER APPEN
-                }
-                else {
-                    korre = korr;
-                }
+                korre = korr;
                 String korreksjon = String.format("%.1f", korre);
                 double ins = korre + kar;
                 final String insulindose = String.format("%.1f", ins);
@@ -80,39 +68,6 @@ public class MainActivity extends AppCompatActivity {
                 InsulindoseTextView.setText("Den totale dosen er " + insulindose + " enheter");
                 KorreksjonTextView.setText("Korreksjonen tilsvarer " + korreksjon + " enheter av dosen");
                 KarbTextView.setText("Karbohydratene tilsvarer " + karbdose + " enheter av dosen");
-
-
-
-                //ACTIVE INSULIN
-
-
-
-                InsulinButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String text = InsuEditText.getText().toString();
-                        if (!text.equalsIgnoreCase("")) {
-                            final int hours = Integer.valueOf(text);
-                            final CountDownTimer TotalTid = new CountDownTimer(hours * 3600000, 60000) //Decides how many milliseconds the countdown will be (3600000 being an hour) and how often it will update (60000 is 1 minute)
-                            {
-                                @Override
-                                public void onTick(long millisUntilFinished) {
-                                    long start_time = System.currentTimeMillis();
-                                    double TimePassed = System.currentTimeMillis() - start_time;
-                                    int time = hours * 3600000;
-                                    Log.d("test", "time: " + time + " , millisUntilFinished: " + millisUntilFinished); //Used this to log the values
-                                    DecimalFormat numberFormat = new DecimalFormat("#.0"); //Limits the amount of decimals shown when outputting the active insulin
-                                    AktivInsulinTextView.setText("Aktiv insulin: " + numberFormat.format(insulinDoseValue / time * millisUntilFinished)); //Calculates and outputs the active insulin
-                                }
-
-                                @Override
-                                public void onFinish() {
-                                    AktivInsulinTextView.setText("Ingen aktiv insulin"); //Text that is shown when the active insulin timer reached 0
-                                }
-                            }.start();
-                        }
-                    }
-                });
             }
         });
     }
